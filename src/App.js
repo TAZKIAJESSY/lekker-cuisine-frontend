f4c1658b4c05e070ad4cdcdb8aff6b771b48f836
tazkiajessy <jessy566eee@gmail.com>
2021-04-06 20:57:23 +0200

    redux store created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-
     <div className="App">
-      <NavBar/>
+      <NavBar />
       <Switch>
-        <Route exact path="/", component={} />
+        <Route exact path="/" component={HomePage} />
       </Switch>
     </div>
   );
