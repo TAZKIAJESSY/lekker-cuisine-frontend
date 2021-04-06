28cf0e996d883a6d69df5e8104262171e38e8da6
tazkiajessy <jessy566eee@gmail.com>
2021-04-06 20:41:14 +0200

    navbar added

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div
+      style={{
+        margin: 20,
+        fontSize: 25,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <NavLink exact to="/" activeStyle={{ color: "teal" }}>
+        Cuisines
+      </NavLink>
+    </div>
+  );
+}
+
+export default NavBar;
