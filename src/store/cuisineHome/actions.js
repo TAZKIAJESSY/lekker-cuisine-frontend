d2a8e39c115812f83d1f7c89c856e0dc978738f0
tazkiajessy <jessy566eee@gmail.com>
2021-04-06 21:13:12 +0200

    thunk call fetch all cusisines

---
+++ src/store/cuisineHome/actions.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { apiUrl } from "../../config/constants";
+
+export function allCuisineFetched(cuisineList) {
+  return { type: "cuisineHome/allCuisineFetched", payload: cuisineList };
+}
+
+export async function fetchcuisineList(dispatch, getState) {
+  try {
+    const getCuisine = getState().cuisineHome.cuisines;
+
+    if (!getCuisine.length) {
+      const response = await axios.get(`${apiUrl}/cuisines`);
+      console.log("All cuisines", response);
+
+      dispatch(allCuisineFetched(response.data));
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+}
