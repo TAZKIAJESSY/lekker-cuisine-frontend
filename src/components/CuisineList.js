88ec8c494718c1309a23d1c3f724beaecb3e3a8d
tazkiajessy <jessy566eee@gmail.com>
2021-04-07 11:09:01 +0200

    some stylling to the  card

--- src/components/CuisineList.js
+++ src/components/CuisineList.js
@@ -2,9 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function CuisineList(props) {
   return (
-    <div>
-      <h2>{props.title}</h2>
-      <img src={props.imgUrl} alt={props.title}></img>
+    <div className="card shadow-lg mb-4">
+      <div className="card-body pb-0">
+        <h5 className="card-title">{props.title}</h5>
+        <img
+          style={{ width: 200, height: 250 }}
+          src={props.imageUrl}
+          alt={props.title}
+        ></img>
+      </div>
     </div>
   );
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,16 +4,26 @@ import { useDispatch, useSelector } from "react-redux";
 import CuisineList from "../../components/CuisineList";
 
 import { fetchcuisineList } from "../../store/cuisineHome/actions";
+import { selectCuisineHome } from "../../store/cuisineHome/selectors";
 
 export default function HomePage() {
   const dispatch = useDispatch();
+  const cuisine = useSelector(selectCuisineHome);
 
   useEffect(() => {
     dispatch(fetchcuisineList);
   }, [dispatch]);
   return (
-    <div>
-      <CuisineList />
+    <div className="container">
+      <div className="row">
+        {cuisine.map((cui, index) => {
+          return (
+            <div className="col-lg-3" key={index}>
+              <CuisineList title={cui.title} imageUrl={cui.imageUrl} />
+            </div>
+          );
+        })}{" "}
+      </div>
     </div>
   );
 }
