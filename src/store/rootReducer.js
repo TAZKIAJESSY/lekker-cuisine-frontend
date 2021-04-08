694fb7a6ce54f836ccf7d1aaffc2f160a5abf723
tazkiajessy <jessy566eee@gmail.com>
2021-04-08 14:36:24 +0200

    appState defined in rootReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import cuisineHome from "./cuisineHome/reducer";
+import appState from "./appState/reducer";
 
 export default combineReducers({
   cuisineHome,
+  appState,
 });
