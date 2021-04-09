ddbbcc1fbe0bf10569c9d0611655065846240610
tazkiajessy <jessy566eee@gmail.com>
2021-04-09 23:52:37 +0200

    favourite page likes button update working

--- src/store/userFav/actions.js
+++ src/store/userFav/actions.js
@@ -1,24 +1,24 @@
-import axios from "axios";
-import { apiUrl } from "../../confiig/constants";
-import { selectUser } from "../user/selectors";
+// import axios from "axios";
+// import { apiUrl } from "../../confiig/constants";
+// import { selectUser } from "../user/selectors";
 
-export function userFavFetched(favouriteList) {
-  return { type: "userFav/userFavFetched", payload: favouriteList };
-}
+// export function userFavFetched(favouriteList) {
+//   return { type: "userFav/userFavFetched", payload: favouriteList };
+// }
 
-export async function fetchFavouriteList(dispatch, getState) {
-  try {
-    const { token } = selectUser(getState());
+// export async function fetchFavouriteList(dispatch, getState) {
+//   try {
+//     const { token } = selectUser(getState());
 
-    const response = await axios.get(`${apiUrl}/favourites`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    console.log("All favourites", response.data);
+//     const response = await axios.get(`${apiUrl}/favourites`, {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     console.log("All favourites", response.data);
 
-    dispatch(userFavFetched(response.data));
-  } catch (e) {
-    console.log(e.message);
-  }
-}
+//     dispatch(userFavFetched(response.data));
+//   } catch (e) {
+//     console.log(e.message);
+//   }
+// }
--- src/store/userFav/reducer.js
+++ src/store/userFav/reducer.js
@@ -1,13 +1,13 @@
-const initialState = {
-  favourites: [],
-};
+// const initialState = {
+//   favourites: [],
+// };
 
-export default function favReducer(state = initialState, action) {
-  switch (action.type) {
-    case "userFav/userFavFetched": {
-      return { favourites: [...action.payload] };
-    }
-    default:
-      return state;
-  }
-}
+// export default function favReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case "userFav/userFavFetched": {
+//       return { favourites: [...action.payload] };
+//     }
+//     default:
+//       return state;
+//   }
+// }
