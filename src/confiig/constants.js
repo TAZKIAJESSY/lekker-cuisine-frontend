702f4b3fbc38ef8512205d50a98fd1c514f15a8e
tazkiajessy <jessy566eee@gmail.com>
2021-04-27 23:30:10 +0200

    use heroku

--- src/confiig/constants.js
+++ src/confiig/constants.js
@@ -1,2 +1,5 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+// export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.API_URL || "https://lekker-cuisineheroku-db.herokuapp.com"; //for heroku deploy
+
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
