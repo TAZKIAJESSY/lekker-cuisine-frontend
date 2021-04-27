db3f761f96abaa3b2184f77db8b833118709a06e
tazkiajessy <jessy566eee@gmail.com>
2021-04-27 22:52:53 +0200

    fetch store function for shop api

--- src/store/nearShop/actions.js
+++ src/store/nearShop/actions.js
@@ -1,27 +1,20 @@
 import axios from "axios";
 
-const API_URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?`;
-const API_KEY = "AIzaSyA3K0C0fExKEAShuqb-Z3Efgmyk1n7y598";
+import { apiUrl } from "../../confiig/constants";
 
 export function fetchedStore(list) {
   return { type: "nearShop/fetchedStore", payload: list };
 }
 
-export const storeDataLoading = () => ({
-  type: "nearShop/loading",
-});
+// export const storeDataLoading = () => ({
+//   type: "nearShop/loading",
+// });
 
-export const fetchStore = (currentLocation) => async (dispatch, getState) => {
-  dispatch(storeDataLoading());
+export const fetchStore = (location) => async (dispatch, getState) => {
+  //dispatch(storeDataLoading());
   try {
-    //console.log("Coordinates: ", currentLocation);
+    const response = await axios.get(`${apiUrl}/shop/${location}`);
 
-    const response = await axios.get(
-      `${API_URL}location=${currentLocation.lattitude},${currentLocation.longtitude}&radius=2000&type=supermarket&key=${API_KEY}&radius=2000&type=supermarket&key=AIzaSyA3K0C0fExKEAShuqb-Z3Efgmyk1n7y598&rankBy?google.maps.places.RankBy.DISTANCE`,
-      {
-        crossdomain: true,
-      }
-    );
     console.log("All shop list", response);
 
     dispatch(fetchedStore(response.data.results));
